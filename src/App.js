6f45e77aa03f3f54808924f065f442447ac10edc
Nathanael Raway <nathanael.raway@gmail.com>
2019-04-03 14:10:08 -0500

    Hardcoded for iPad so I can use it

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
                         rel="noopener noreferrer"
                     >
                         youcansithere.com
-          </a>
+                    </a>
                 </header>
             </div>
         );
