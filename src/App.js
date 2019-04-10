ecfce2cf1f51221ea92ae26054a54f934455af92
Nathanael Raway <nathanael.raway@gmail.com>
2019-04-10 11:05:52 -0500

    Use StandardJS

--- src/App.js
+++ src/App.js
@@ -1,38 +1,35 @@
-import React, { Component } from 'react';
-import 'bulma';
-import './App.css';
-const faChair = require('./chair-solid.svg');
+import React, { Component } from 'react'
+import 'bulma'
+import './App.css'
+const faChair = require('./chair-solid.svg')
 
-const message = 'you can sit here';
+const message = 'you can sit here'
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img className=" App-logo" src={faChair} alt="chair" />
-                    <h1>{message}</h1>
-                    <a
-                        className="App-link"
-                        href="https://youcansithere.com"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        youcansithere.com
-                    </a>
-                    <p className="is-size-6">
-                    Created by <a
-                        className="App-link is-pull-right"
-                        href="https://nathanaelraway.com"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >Nathanael Raway</a>
-                    </p>
-
-                </header>
-            </div>
-        );
-    }
+  render () {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <img className='App-logo' src={faChair} alt='chair' />
+          <h1>{message}</h1>
+          <a
+            className='App-link'
+            href='https://youcansithere.com'
+            target='_blank'
+            rel='noopener noreferrer'
+          >youcansithere.com</a>
+          <p className='is-size-6'>
+            Created by <a
+              className='App-link is-pull-right'
+              href='https://nathanaelraway.com'
+              target='_blank'
+              rel='noopener noreferrer'
+            >Nathanael Raway</a>
+          </p>
+        </header>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
